4cddb236eb8ee6afa60d7f50228b2df6c5f658b8
Ruben Paz Chuspe <rubenpazchuspe@outlook.com>
2021-04-05 12:07:38 -0500

    test solution

--- lib/dice.rb
+++ lib/dice.rb
@@ -1,10 +1,30 @@
 class Dice
 
-  def initialize(sided = 6)
+  def initialize(sided = 6, default_value=nil)
     @total_sides = sided
+    @default_value = default_value
+  end
+  
+  def loaded?
+    !@default_value.nil?
+  end
+  
+  def roll
+    return loaded_roll if loaded?
+    return random_roll unless loaded?
   end
 
-  def roll
+  def loaded_roll 
+    temp = Random.new
+    newNUmber = temp.rand(1..10) 
+    if newNUmber > 5
+      @default_value
+    else 
+      random_roll
+    end
+  end
+  
+  def random_roll
     prgn = Random.new
     prgn.rand(1..@total_sides)
   end
--- spec/lib/dice_spec.rb
+++ spec/lib/dice_spec.rb
@@ -6,7 +6,34 @@ RSpec.describe Dice do
         dice = Dice.new(8)
         expect(dice.roll).to be_between(1, 8)
       end
+      it 'it create a loaded die' do
+        dice = Dice.new(8, 3)
+        expect(dice.loaded?).to eql(true)
+      end
+      it 'it create non loaded die' do
+        dice = Dice.new(8, nil)
+        expect(dice.loaded?).to eql(false)
+      end
+      it 'when loaded should return loaded value half the time' do
+        dice = Dice.new(8, 3)
+        total_thre = 0
+        1000.times do
+          if  dice.roll == 3
+            total_thre += 1
+          end
+        end
+        expect(total_thre).to be_between(400, 600)
+      end
+      it 'when loaded should return loaded value half the time' do
+      end
   end
+  
+  #describe 'Dice Class Forcing number' do
+  #  it 'returns a number 3 half the time' do
+  #    dice = Dice.new(8)
+  #    expect(dice.roll).to eql(3)
+  #  end
+  #end
 end
 
 
